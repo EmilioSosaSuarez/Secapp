1bf15b7a5c55e17e94cecb240d50111d30b8e83c
Alexissiminkowich <alexis.frlp@gmail.com>
2019-11-20 11:24:33 -0300

    Agrego controlador Dictado de Capacitaciones

--- app/CapacitacionesDictado.php
+++ app/CapacitacionesDictado.php
@@ -4,13 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Capacitaciones extends Model
+class CapacitacionesDictado extends Model
 {
-    protected $table = 'capacitaciones';
+    protected $table = 'capacitacionesDictado';
     protected $fillable = [ 'id',
 							'nombre',
+							'capacitador',
+							'fecha',
 							'temario',
 							'duracion',
-							'extra'
-							'fechaAlta'];
+							'participantes'];
 }
