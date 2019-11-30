97e5e02086b51ac2477c1415ce81811cb72d4698
Emilio Sosa Suárez <sosasuarez.emilio@gmail.com>
2019-11-30 19:20:37 -0300

    Update busquedaResultado.blade.php

--- resources/views/admin/persona/busquedaResultado.blade.php
+++ resources/views/admin/persona/busquedaResultado.blade.php
@@ -30,61 +30,58 @@
 @section('busquedaResultado')
 
 <!-- page búsqueda resultado-->
-    <div class="col-md-12 col-sm-12 col-xs-12">
-    <div class="x_panel">
-    <div class="x_content">
+	<div class="col-md-12 col-sm-12 col-xs-12">
+	<div class="x_panel">
+	<div class="x_content">
 
-    <table id="datatable" class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th>DNI</th>
-          <th>Apellido</th>
-          <th>Nombre</th>
-          <th>Email</th>
-          <th>Celular</th>
-          <th>Fecha Alta</th>
-          <th>Fecha Baja</th>
-          <th></th>
-        </tr>
-      </thead>
+	<table id="datatable" class="table table-striped table-bordered">
+	  <thead>
+	    <tr>
+	      <th>DNI</th>
+	      <th>Apellido</th>
+	      <th>Nombre</th>
+	      <th>Email</th>
+	      <th>Celular</th>
+	      <th>Fecha Alta</th>
+	      <th>Fecha Baja</th>
+	      <th></th>
+	    </tr>
+	  </thead>
 
-      <tbody>
-        @foreach($personas as $persona)
-            <tr>
-              <td>{{ $persona->dni }}</td>
-              <td>{{ $persona->apellido }}</td>
-              <td>{{ $persona->nombre }}</td>
-              <td>{{ $persona->email }}</td>
-              <td>{{ $persona->celular }}</td>
-              <td>{{ $persona->fechaAlta }}</td>
-              <td>
-                  @if($persona->fechaBaja === NULL) <!-- Si la "fecha de baja" es NULL -->
-                        <input type="date" class="form-control has-feedback-left" id='{{ $persona->dni }}' placeholder="Fecha Baja" aria-describedby="inputSuccess2Status">
-                  @else <!-- Si la "fecha de baja" NO es NULL entonces le doy formato al valor fecha-->
-                        <input value= '{{ date('Y-m-d', strtotime($persona->fechaBaja)) }}'
-                        type="date" class="form-control has-feedback-left" id='{{ $persona->dni }}' placeholder="Fecha Baja" aria-describedby="inputSuccess2Status">
-                  @endif
-              </td>
-              <td>
+	  <tbody>
+		@foreach($personas as $persona)
+			<tr>
+		      <td>{{ $persona->dni }}</td>
+		      <td>{{ $persona->apellido }}</td>
+		      <td>{{ $persona->nombre }}</td>
+		      <td>{{ $persona->email }}</td>
+		      <td>{{ $persona->celular }}</td>
+		      <td>{{ $persona->fechaAlta }}</td>
+		      <td>
+			      @if($persona->fechaBaja === NULL) <!-- Si la "fecha de baja" es NULL -->
+		                <input type="date" class="form-control has-feedback-left" id='{{ $persona->dni }}' placeholder="Fecha Baja" aria-describedby="inputSuccess2Status">
+			      @else <!-- Si la "fecha de baja" NO es NULL entonces le doy formato al valor fecha-->
+		                <input value= '{{ date('Y-m-d', strtotime($persona->fechaBaja)) }}'
+		                type="date" class="form-control has-feedback-left" id='{{ $persona->dni }}' placeholder="Fecha Baja" aria-describedby="inputSuccess2Status">
+		          @endif
+		      </td>
+		      <td>
                 <a href="{{ route('admin.persona.ver', $persona->id) }}" target="_blank" class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> Ver </a>
                 <a href="{{ route('admin.persona.edit', $persona->id) }}" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Editar </a>
-                <form method="POST" action="{{ route('admin.persona.destroy', $persona->id) }}" style="display: inline;">
-                @csrf @method('DELETE')
-                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Eliminar </button>
-                </form>
+                <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Eliminar </a>
               </td>
-            </tr>
-        @endforeach
-      </tbody>
-    </table>
+			</tr>
+		@endforeach
+	  </tbody>
+	</table>
 
-    </div>
-    </div>
-    </div>
+	</div>
+	</div>
+	</div>
 <!-- /page búsqueda resultado-->
 
 
-    <!-- jQuery -->
+	<!-- jQuery -->
     <script src="theme/vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
     <script src="theme/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
