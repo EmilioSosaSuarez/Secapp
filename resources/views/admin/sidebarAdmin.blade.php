08a9f1cd6b66ee39f79e97e3361875336849b717
Emilio Sosa Suárez <sosasuarez.emilio@gmail.com>
2019-11-30 19:24:13 -0300

    Update sidebarAdmin.blade.php

--- resources/views/admin/sidebarAdmin.blade.php
+++ resources/views/admin/sidebarAdmin.blade.php
@@ -9,7 +9,7 @@
         </ul>
       </li>
       <li><a href="projects.html"><i class="fa fa-files-o"></i> Contratos </a>
-      <! Incidentes   >
+      <!-- Incidentes -->
             <li><a><i class="fa fa-exclamation-triangle"></i> Incidentes <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
           <li><a href="{{ route('admin.incidente.create') }}">Alta Incidente</a></li>
@@ -21,7 +21,7 @@
             <li><a href="{{ route('admin.capacitaciones.busquedaIndex') }}">Búsqueda</a></li>
         </ul>
       </li>
-      <! Fin Incidentes   >
+      <!-- /Fin Incidentes -->
 
       </li>
     </ul>
